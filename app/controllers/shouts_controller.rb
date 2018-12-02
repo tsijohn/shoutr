fcafe474373275793fbc0c568085851c28cc5775
John Kim <utjohnkkim@gmail.com>
2018-12-02 16:14:20 -0600

    Allow user to shout

---
+++ app/controllers/shouts_controller.rb
@@ -0,0 +1,18 @@
+class ShoutsController < ApplicationController
+  def create
+    shout = current_user.shouts.create(shout_params)
+    redirect_to root_path, redirect_options_for_shout(shout)
+  end
+
+  def shout_params
+    params.require(:shout).permit(:body)
+  end
+
+  def redirect_options_for_shout(shout)
+    if shout.persisted?
+      { notice: "Shouted successfully"}
+    else
+      { alert: "Could not shout"}
+    end
+  end
+end
